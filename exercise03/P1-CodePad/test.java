4eb5819f647761b4acdd77cd444314450372dfab
Artjom Schwenk <artjom.schwenk@student.htw-berlin.de>
2023-10-29 00:46:40 +0200

    Project so far

---
+++ exercise03/P1-CodePad/test.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class test here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class test
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class test
+     */
+    public test()
+    {
+
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void sampleMethod()
+    {
+        System.out.println(99 + 3);
+        System.out.println("cat" + "fish");
+        System.out.println("cat" + 9);
+        System.out.println(9 + 3 + "cat");
+        System.out.println("cat" + 3 + 9 + "fish");
+        System.out.println("catfish".substring(3,4));
+        System.out.println("catfish".substring(3,7));
+    }
+}
